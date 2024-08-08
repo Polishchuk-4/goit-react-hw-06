4d9b49b48a1e35200f80dd03d869f97892685ac3
Denys Polishchuk <polischyk0000@gmail.com>
2024-08-08 17:05:40 +0300

    add whitelist contacts

--- src/redux/store.js
+++ src/redux/store.js
@@ -12,7 +12,7 @@ const rootReducer = combineReducers({
 const persistConfig = {
   key: "root",
   storage,
-  whitelist: ["items"],
+  whitelist: ["contacts"],
 };
 
 const persisedReducer = persistReducer(persistConfig, rootReducer);
